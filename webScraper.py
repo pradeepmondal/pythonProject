2dfe064af47b8fd37872497ce0dfcc961795e845
pradeepmondal <mail.pradeepmondal@gmail.com>
2022-11-02 23:28:00 +0530

    updated the file

--- webScraper.py
+++ webScraper.py
@@ -1,1 +1,8 @@
 # This is a python web scraper
+import requests
+from bs4 import BeautifulSoup as b
+
+gp = "https://github.com/pradeepmondal"
+req = requests.get(gp)
+scp = b(req.content, "html.parser")
+
